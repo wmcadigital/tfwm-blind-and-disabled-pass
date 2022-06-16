fc50c79f6f59befd39006c6bd7490aca5d2e101c
Mushtaque Ahmed <mushtaque.ahmed@wmca.org.uk>
2022-06-16 22:56:32 +0100

    Disable pass update

--- src/components/App/Form/Questions/Questions.tsx
+++ src/components/App/Form/Questions/Questions.tsx
@@ -1,4 +1,5 @@
 import { useGlobalContext } from 'state/globalState';
+import { useFormDataSubscription } from 'customHooks';
 
 // Sections
 import SectionOne from './Sections/1-SectionOne/SectionOne';
@@ -6,13 +7,16 @@ import SectionTwo from './Sections/2-SectionTwo/SectionTwo';
 import SectionThree from './Sections/3-SectionThree/SectionThree';
 import SectionFour from './Sections/4-SectionFour/SectionFour';
 
-const sections = [SectionOne, SectionTwo, SectionThree, SectionFour];
-
 const Form = () => {
   const [globalState] = useGlobalContext();
+  const applicationForMe = useFormDataSubscription('applicationForMe');
   const { form } = globalState;
+  const sections = applicationForMe.savedValue
+    ? [SectionOne, SectionTwo, SectionThree]
+    : [SectionOne, SectionTwo, SectionThree, SectionFour];
 
   const totalSections = sections.length;
+
   const SectionToShow = sections[form.currentSection - 1];
 
   return <SectionToShow totalSections={totalSections} />;
--- src/components/shared/Section/Section.tsx
+++ src/components/shared/Section/Section.tsx
@@ -6,10 +6,23 @@ const Section = ({ totalSections, title, steps }: TProps) => {
   const { currentSection, currentStep } = globalState.form;
 
   const StepToShow = steps[currentStep - 1];
-
+  const options = () => {
+    if (currentSection === 3 && totalSections === 4) {
+      return '3';
+    }
+    if (currentSection === 4 && totalSections === 4) {
+      return '2';
+    }
+    if (currentSection === 3 && totalSections === 3) {
+      return '2';
+    }
+    return '1';
+  };
   return (
     <div>
-      <p className="wmnds-m-b-none">{`Section ${currentSection} of ${totalSections}`}</p>
+      {currentSection !== 1 && (
+        <p className="wmnds-m-b-none">{`Section ${options()} of ${totalSections - 1}`}</p>
+      )}
       <strong>{title}</strong>
       <StepToShow />
     </div>
