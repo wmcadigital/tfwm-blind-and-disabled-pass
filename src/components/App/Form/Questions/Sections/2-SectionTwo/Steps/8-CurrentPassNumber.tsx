266c79bf36c594c2b0f961b242c51f3bf96eb36d
Mushtaque Ahmed <mushtaque.ahmed@wmca.org.uk>
2022-06-20 16:43:14 +0100

    Disable pass update

--- src/components/App/Form/Questions/Sections/2-SectionTwo/Steps/8-CurrentPassNumber.tsx
+++ src/components/App/Form/Questions/Sections/2-SectionTwo/Steps/8-CurrentPassNumber.tsx
@@ -3,12 +3,13 @@ import { CurrentPassStep } from 'components/sharedSteps';
 
 const CurrentPass = () => {
   const { goToNextStep } = useNavigationLogic('CurrentPass', 'ChangePhoto');
+  const ApplicantFirstName = useFormDataSubscription('ApplicantFirstName');
 
   const applicationForMe = useFormDataSubscription('applicationForMe');
 
   const question = applicationForMe.currentValue
     ? `What is your current disabled person's pass number?`
-    : `What is your current disabled person's pass number?`;
+    : `What is ${ApplicantFirstName.currentValue}'s current disabled person's pass number?`;
 
   const dataNamePrefix = applicationForMe.currentValue ? 'Applicant' : 'Behalf';
 
