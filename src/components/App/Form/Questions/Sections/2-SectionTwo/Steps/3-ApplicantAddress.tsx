15926c462de5f2cd7bdae26416b97b2875703480
Mushtaque Ahmed <mushtaque.ahmed@wmca.org.uk>
2022-11-16 11:23:39 +0000

    Adding postcode exceptions

--- src/components/App/Form/Questions/Sections/2-SectionTwo/Steps/3-ApplicantAddress.tsx
+++ src/components/App/Form/Questions/Sections/2-SectionTwo/Steps/3-ApplicantAddress.tsx
@@ -4,20 +4,23 @@ import { AddressStep } from 'components/sharedSteps';
 const ApplicantAddress = () => {
   const ApplicantCurrentPostcode = useFormDataSubscription('ApplicantCurrentPostcode').savedValue;
   const applicationForMe = useFormDataSubscription('applicationForMe');
+  const postcodeLetter =
+    ApplicantCurrentPostcode && ApplicantCurrentPostcode.substring(0, 3).toUpperCase();
   const postcodeWV =
     ApplicantCurrentPostcode && ApplicantCurrentPostcode.substring(0, 2).toUpperCase();
   const postcodeB =
     ApplicantCurrentPostcode && ApplicantCurrentPostcode.substring(0, 1).toUpperCase();
   const postcodeNumber = ApplicantCurrentPostcode ? ApplicantCurrentPostcode.substring(1, 2) : '';
   const isDigit = (n: string) => /\d+/.test(n);
-  const nextStep =
+  const exceptions =
+    postcodeLetter === 'CV8' || postcodeLetter === 'B46' || postcodeLetter === 'WV5';
+  const postcodeSort =
     postcodeWV === 'WV' ||
     postcodeWV === 'CV' ||
     postcodeWV === 'DY' ||
     postcodeWV === 'WS' ||
-    (postcodeB === 'B' && isDigit(postcodeNumber))
-      ? 'ApplicantEthnicity'
-      : 'NotEligiblePostCode';
+    (postcodeB === 'B' && isDigit(postcodeNumber));
+  const nextStep = postcodeSort && !exceptions ? 'ApplicantEthnicity' : 'NotEligiblePostCode';
   const { goToNextStep } = useNavigationLogic('ApplicantBirthDate', nextStep);
   const ApplicantFirstName = useFormDataSubscription('ApplicantFirstName');
   const pronoun = applicationForMe.savedValue ? 'your' : `${ApplicantFirstName.currentValue}'s`;
