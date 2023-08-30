4ffa229ca3e391ca0a0dc669035e6984df9ee6ac
Nathan Rogan <rolorogan@gmail.com>
2023-08-30 11:04:32 +0100

    Next release (#116)
    * fix ssl start
    
    * add displayname to email api (#112)
    
    * Nr/feat/sd262606 (#115)
    
    * remove node-sass
    
    * fix sass format
    
    * add one instead of at least two
    
    * fix eslint issue
    
    * update upload txt on proof step

--- src/customHooks/axiosRequests/_useAxiosRequest/useAxiosRequest.types.ts
+++ src/customHooks/axiosRequests/_useAxiosRequest/useAxiosRequest.types.ts
@@ -1,8 +1,6 @@
 import { AxiosRequestConfig, AxiosResponse } from 'axios';
 
-export type TUseAxiosRequest = <T>(
-  axiosConfig: AxiosRequestConfig,
-) => {
+export type TUseAxiosRequest = <T>(axiosConfig: AxiosRequestConfig) => {
   isLoading: boolean;
   hasError: boolean;
   response: AxiosResponse<T> | null;
